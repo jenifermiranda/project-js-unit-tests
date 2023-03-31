97a9d663213b8a8c1918b4fed4a4136253ccb9de
Jenifer <jnf.asm@gmail.com>
2023-03-30 21:33:13 -0300

    requisito 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -44,13 +44,29 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // 1. Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // 2. Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    // 3. Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
-    // 4. Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    // 5. Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
-    // 6. Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('arya')).toStrictEqual({"class": "Rogue", "name": "Arya Stark", "phrases": ["Not today", "A girl has no name."]});
   });
+  it('Verifica se quando não receber um parâmetro a função `getCharacter` retorna `undefined`', () => {
+    expect(getCharacter()).toBeUndefined();
+  });
+  it('Verifica se a função `getCharacter` retorna o objeto correto para o parâmetro `Arya`', () => {
+    expect(getCharacter('Arya')).toMatchObject({"class": "Rogue", "name": "Arya Stark", "phrases": ["Not today", "A girl has no name."]});
+  });
+  it('Verifica se a função `getCharacter` retorna o objeto correto para o parâmetro `Brienne`', () => {
+    expect(getCharacter('Brienne')).toMatchObject({"class": "Knight", "name": "Brienne Tarth", "phrases": ["Im No Lady, Your Grace.", "I, Brienne Of Tarth, Sentence You To Die."]});
+  });
+  it('Verifica se a função `getCharacter` retorna o objeto correto para o parâmetro `Melissandre`', () => {
+    expect(getCharacter('Melissandre')).toMatchObject({"class": "Necromancer", "name": "Melissandre", "phrases": ["Death By Fire Is The Purest Death.", "For The Night Is Dark And Full Of Terrors."]});
+  });
+  it('Verifica se o parâmetro não é Case Sensitive', () => {
+    expect(getCharacter('arYa')).toStrictEqual({"class": "Rogue", "name": "Arya Stark", "phrases": ["Not today", "A girl has no name."]});
+  });
+  it('Verifica se aopassar um nome que não está na tabela, a função retorna `undefined`', () => {
+    expect(getCharacter(undefined)).toBe(undefined);
+  });
+  
+    // ESCREVA SEUS TESTES ABAIXO:
+
+    // 5. Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
+  
 });
